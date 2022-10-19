d6e90640bb10ca825aaa962c821430840b6adc2e
gokdefterim <mustafaakalfa34@gmail.com>
2022-10-20 00:28:01 +0300

    JS ile root degistirildi

---
+++ script.js
@@ -0,0 +1,4 @@
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--dark-blue', 'hsl(220, 16%, 96%)');
+    r.style.setProperty('--light-grayish-blue', 'hsl(233, 26%, 24%)');
