b70abdb316c38151561cbed442419adfaf6501ba
gokdefterim <mustafaakalfa34@gmail.com>
2022-10-23 00:06:18 +0300

    root değiştirme fonksiyonu ve event listener eklendi, güneş iconu eklenmeye çalışıldı

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-    var r = document.querySelector(':root');
-    var rs = getComputedStyle(r);
-    r.style.setProperty('--dark-blue', 'hsl(220, 16%, 96%)');
-    r.style.setProperty('--light-grayish-blue', 'hsl(233, 26%, 24%)');
+    var $rootElement = document.querySelector(':root')|| {}; // fallback
+    var rootStyle = getComputedStyle($rootElement);
+    var storageName = 'mode';
+    $rootElement.style.setProperty('--dark-blue', 'hsl(220, 16%, 96%)');
+    $rootElement.style.setProperty('--light-grayish-blue', 'hsl(233, 26%, 24%)');
+
+    function setMode(mode) {
+        localStorage.setItem(storageName, mode);
+
+        if (mode === 'dark') {
+            $rootElement.style.setProperty('--dark-blue', 'hsl(220, 16%, 96%)');
+            $rootElement.style.setProperty('--light-grayish-blue', 'hsl(233, 26%, 24%)');
+            $('.dark-mode-symbol').html('wb_sunny');
+        } else {
+            $rootElement.style.setProperty('--dark-blue', 'hsl(233, 26%, 24%)');
+            $rootElement.style.setProperty('--light-grayish-blue', 'hsl(220, 16%, 96%)');
+            $('.dark-mode-symbol').html('dark_mode');
+        }
+
+    };
+
+    function setEventListeners(){
+        $('#dark-mode').ready(function() {
+            var $button = document.getElementById("dark-mode")
+            $button.addEventListener("click", function() {
+
+                localStorage.getItem(storageName) === 'dark' ?  setMode('light') : setMode('dark');
+                
+            });
+        });
+
+    };
+
+    setEventListeners();
+    setMode(localStorage.getItem(storageName));
